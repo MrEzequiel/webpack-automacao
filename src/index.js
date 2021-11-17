87cd754fa82dcb660a50856a0d19a4bb0f286731
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2021-11-16 21:43:34 -0500

    prettier config and scss loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import '../scss/style.scss'
+
 import Modal from './modules/Modal'
 
 const button = document.querySelector('.start button')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,24 +8,31 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
     rules: [
       {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
+
+  plugins: [new MiniCssExtractPlugin()],
 
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist')
-    }
-  }
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+  },
 }
